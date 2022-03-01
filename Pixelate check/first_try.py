cf2dadf10e2ab48f5dce8eb72fb0e3c6d3a46ead
LemonRhombus <new3980@gmail.com>
2022-03-01 21:49:05 +0700

    First try

---
+++ Pixelate check/first_try.py
@@ -0,0 +1,10 @@
+#This one will be just checking how pixelate fucntions
+
+from pixelate import pixelate
+
+input = "D:\\GIT\\Pixelar-Visualizar\\Pixelate check\\phone.jpg"
+output = "D:\\GIT\\Pixelar-Visualizar\\Pixelate check\\phone-edited-1.jpg"
+
+pixelate(input,output,5)
+
+#WTF I thought it would be something more complicated

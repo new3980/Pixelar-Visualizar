dff7f878d684e4d2243ae64f82c922ab5068aa01
LemonRhombus <new3980@gmail.com>
2022-03-06 23:45:26 +0700

    interesting stuff

---
+++ ploy-brightness.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+import numpy as np
+
+image = cv.imread("D:\\GIT\\Pixelar-Visualizar\\cv2-interpolate\\zutomayo.jpg")
+
+new_image = np.zeros(image.shape, image.dtype)
+alpha = 1.0 # Simple contrast control
+beta = 0    # Simple brightness control
+# Initialize values
+print(' Basic Linear Transforms ')
+print('-------------------------')
+try:
+    alpha = float(input('* Enter the alpha value [1.0-3.0]: '))
+    beta = int(input('* Enter the beta value [0-100]: '))
+except ValueError:
+    print('Error, not a number')
+# Do the operation new_image(i,j) = alpha*image(i,j) + beta
+# Instead of these 'for' loops we could have used simply:
+# new_image = cv.convertScaleAbs(image, alpha=alpha, beta=beta)
+# but we wanted to show you how to access the pixels :)
+for y in range(image.shape[0]):
+    for x in range(image.shape[1]):
+        for c in range(image.shape[2]):
+            new_image[y,x,c] = np.clip(alpha*image[y,x,c] + beta, 0, 255)
+cv.imshow('Original Image', image)
+cv.imshow('New Image', new_image)
+# Wait until user press some key
+cv.waitKey()
